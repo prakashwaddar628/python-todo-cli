a9ff07a193a2db7ce7d7961de87f45bdc9e450e1
prakashwaddar628 <prakashwaddar628@gmail.com>
2025-06-04 18:50:27 +0530

    project initilized

---
+++ todo.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+
+TODO_FILE = 'tasks.txt'
+COMPLETE_MARKER = '[X]'
+PENDING_MARKER = '[ ]'
+
+def load_tasks():
+    pass
+
+def save_tasks(tasks):
+    pass
+
+def display_tasks(index, tasks):
+    pass
+
+def add_tasks(args):
+    pass
+
+def list_tasks(args):
+    pass
+
+def complete_tasks(args):
+    pass
+
+def delete_tasks(args):
+    pass
+
+def main():
+    pass
